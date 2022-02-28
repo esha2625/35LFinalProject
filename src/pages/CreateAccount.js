0215138b3f1755ac96c4881b41315b0195ef196d
bye20 <byeman20@gmail.com>
2022-02-27 17:46:31 -0800

    Fixed CreateAccountPage and Implemented Duplicate UserNameChecking

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,25 +1,70 @@
-import NewConfessionForm from "../components/confessions/createAccount";
+import NewAccountForm from "../components/confessions/createAccount";
 import {useNavigate} from 'react-router-dom';
-function CreateConfessionsPage() {
+import { useEffect, useState } from "react";
+function CreateAccountPage() {
     const navigate = useNavigate();
-    function addConfessionHandler(confessionData) {
+    var i =1 ;
+    function postrequest(confessionData){
+    fetch(
+        'https://bruinfessions-e55f6-default-rtdb.firebaseio.com/login.json',
+                            {
+                                method: 'POST',
+                                body: JSON.stringify(confessionData),
+                                headers: {
+                                    'Content-Type' : 'application/json'
+                                }
+                            }
+                        ).then(() => {
+                            navigate("/", {replace:true});
+                        });
+                    }
+                
+    
+
+     function addConfessionHandler(confessionData) {
+        
         fetch(
-            'https://bruinfessions-e55f6-default-rtdb.firebaseio.com/confessions.json',
-            {
-                method: 'POST',
-                body: JSON.stringify(confessionData),
-                headers: {
-                    'Content-Type' : 'application/json'
+            'https://bruinfessions-e55f6-default-rtdb.firebaseio.com/login.json'
+            
+        )
+        .then(response => {return response.json();})
+        .then(responseData => {
+            for (const key of Object.entries(responseData)) {
+                console.log(key[1].login);
+                
+                if(key[1].login == confessionData.login){
+                    console.log(key[1].login);
+                    navigate("/", {replace:true});
+                    i =0;
+                    return;
+                    
                 }
+                
             }
-        ).then(() => {
-            navigate("/", {replace:true});
+            if(i != 0){
+                postrequest(confessionData);
+            }
+            
+            
         });
+        
+        
+        
+                        
     }
+            
+              
+              
+        
+        
+
+        
+        
+    
     return <section>
         <h1>Ready to Confess?</h1>
-        <NewConfessionForm onAddConfession = {addConfessionHandler}/>
+        <NewAccountForm onAddConfession = {addConfessionHandler}/>
     </section>
 }
 
-export default CreateConfessionsPage;
+export default CreateAccountPage;
