1d7917727480e0fa84b66ceaa3c55cea94d4e883
bye20 <byeman20@gmail.com>
2022-03-02 17:40:14 -0800

    Added Sortby Newest, Oldest, and Most Likes

--- src/pages/AllConfessions.js
+++ src/pages/AllConfessions.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import ConfessionList from "../components/confessions/ConfessionList";
-import { getDatabase, ref, child, get } from "firebase/database";
+import { getDatabase, ref, child, get, query, orderByKey, orderByChild } from "firebase/database";
 import "./dropdown.css"
 
 function AllConfessionsPage(){
@@ -63,6 +63,77 @@ function AllConfessionsPage(){
       </section>
     );
   }
+  function NewHandler(){
+    
+    const db = getDatabase();
+    const newpost = query(ref(db, 'confessions'), orderByKey());
+    get(newpost).then(response => {
+      return response.val();
+    }).then(data => {
+      const confessions = [];
+
+      for (const key in data) {
+        const confession = {
+          id: key,
+          ...data[key]
+      };
+
+      confessions.push(confession);
+    }console.log(confessions);
+
+    
+    setLoadedConfessions(confessions);
+  });
+  }
+  function OldHandler(){
+    
+    const db = getDatabase();
+    const oldpost = query(ref(db, 'confessions'), orderByKey());
+    get(oldpost).then(response => {
+      return response.val();
+    }).then(data => {
+      const confessions = [];
+
+      for (const key in data) {
+        const confession = {
+          id: key,
+          ...data[key]
+      };
+
+      confessions.push(confession);
+    }
+    confessions.reverse();
+    console.log(confessions);
+
+    
+    setLoadedConfessions(confessions);
+  });
+    
+
+  }
+  function LikeHandler(){
+    const db = getDatabase();
+    const oldpost = query(ref(db, 'confessions'), orderByChild("likes"));
+    get(oldpost).then(response => {
+      return response.val();
+    }).then(data => {
+      const confessions = [];
+
+      for (const key in data) {
+        const confession = {
+          id: key,
+          ...data[key]
+      };
+
+      confessions.push(confession);
+    }
+    
+
+    
+    setLoadedConfessions(confessions);
+  });
+
+  }
 
   return (
   <section>
@@ -71,9 +142,9 @@ function AllConfessionsPage(){
       <div className="dropdown">
         <button className="dropbtn">Sort</button>
         <div className="dropdown-content">
-          <a href="#">Newest</a>
-          <a href="#">Oldest</a>
-          <a href="#">Most Liked</a>
+          <a href="#" onClick= {NewHandler}>Newest</a>
+          <a href="#"onClick= {OldHandler}>Oldest</a>
+          <a href="#" onClick= {LikeHandler}>Most Liked</a>
         </div>
       </div>
       <ConfessionList confessions={loadedConfessions} />
