8685e78276677d237e69dda0ae5d843099ae34c3
Kat Nasif <katherine.nasif@gmail.com>
2022-03-09 19:32:43 -0800

    Updated Dynamic MainNavigation
    When user is logged in, navigation bar doesn't let them see options to favorite  / post confessions.

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -6,7 +6,6 @@ function MainNavigation() {
     return (
     <header className={classes.header}>
             <div className={classes.logo_wrap}>
-                 <div className={classes.logoimage}><img src="/bruinfessionslogo.png" alt="Bruinfessions" width="90ref" height="5ref"/></div> 
                 <div className={classes.logo}>Bruinfessions</div>
             </div>
         <nav>
@@ -14,12 +13,6 @@ function MainNavigation() {
             <ul>
                 <li>
                     <Link to = '/'>All Confessions</Link>
-                </li>
-                <li>
-                    <Link to = '/create-confession'>Create Confessions</Link>
-                </li>
-                <li>
-                    <Link to = '/favorites'>Favorites</Link>
                 </li>
                 <li>
                     <Link to = '/login-page'>Login</Link>
